a643453824f7497dfc72746e01683cdbd8e7253a
dvp22vsu <dvp22vsu@uea.ac.uk>
2024-02-27 11:49:06 +0000

    Tidying of DATA

--- scripts/ANOVA.R
+++ scripts/ANOVA.R
@@ -1,1 +1,15 @@
+# Packages:
+library(here)
+library(tidyverse)
+
+# Import Data:
 Frogs <- read_csv(here("data", "frogs_messy_data.csv"))
+
+# Tidy Data:
+Frogs <- Frogs %>%
+  rename("13" = Temperature13,
+         "18" = Temperature18,
+         "25" = Temperature25, 
+         Frogspawn_ID = `Frogspawn sample id`) %>% 
+  pivot_longer(`13`:`25`, names_to="temperature", values_to="days") %>%
+  drop_na(days)
